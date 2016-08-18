4e50b2e9add204453afbee722776caac0f1fe13b
4ndersonLin <gesarlin0803@gmail.com>
2016-08-18 22:06:50 +0800

    new ver

--- index-page/index.php
+++ index-page/index.php
@@ -1,13 +1,21 @@
 <?php
-$url = "http://169.254.169.254/latest/meta-data/local-ipv4";
+$url1 = "http://169.254.169.254/latest/meta-data/local-ipv4";
+$url2 = "http://169.254.169.254/latest/meta-data/"; 
+$ch1 = curl_init();
  
-$ch = curl_init();
+curl_setopt($ch1, CURLOPT_URL, $url1);
+curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
+$output1 = curl_exec($ch1);
  
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$output = curl_exec($ch);
+curl_close($ch1);
+$ch2 = curl_init();
  
-curl_close($ch);
+curl_setopt($ch2, CURLOPT_URL, $url2);
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+$output2 = curl_exec($ch2);
  
-echo $output;
+curl_close($ch12;
+ 
+echo $output1;
+echo $output2;
 ?>
