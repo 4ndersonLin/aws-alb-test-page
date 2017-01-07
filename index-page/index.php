fe72818448c5d4ff157fed3a239b73627dc7dd1e
4ndersonLin <gesarlin0803@gmail.com>
2017-01-07 23:40:11 +0800

    index.fix

--- index-page/index.php
+++ index-page/index.php
@@ -1,6 +1,6 @@
 <?php
 $url1 = "http://169.254.169.254/latest/meta-data/local-ipv4";
-$url2 = "http://169.254.169.254/latest/meta-data/"; 
+$url2 = "http://169.254.169.254/latest/meta-data/hostname"; 
 $ch1 = curl_init();
  
 curl_setopt($ch1, CURLOPT_URL, $url1);
@@ -14,7 +14,7 @@ curl_setopt($ch2, CURLOPT_URL, $url2);
 curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
 $output2 = curl_exec($ch2);
  
-curl_close($ch12;
+curl_close($ch2);
  
 echo $output1;
 echo $output2;
